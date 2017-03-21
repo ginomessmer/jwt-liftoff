2c57e59501fee251bd747b7699444e999ead51ba
Gino Messmer <g.messmer@ftsoftware.eu>
2017-03-21 10:20:26 +0100

    JWT app configuration

--- src/JwtLiftoff/Services/ConfigurationService.cs
+++ src/JwtLiftoff/Services/ConfigurationService.cs
@@ -6,15 +6,24 @@ using System.Collections.Generic;
 using JwtLiftoff.Data;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using Microsoft.AspNetCore.Builder;
 
 namespace JwtLiftoff.Services
 {
     public class ConfigurationService
     {
+        // Keep this secret, will ya
+        internal static readonly SymmetricSecurityKey SigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes("TotallySecret"));
+
+        internal const string ISSUER = "UberJwtServer";
+        internal const string AUDIENCE = "https://localhost/";
+
+        #region Methods
         public static void AddMvcConfig(IServiceCollection services)
         {
             services.AddMvc(config => 
             {
+                // Require auth per default
                 var policy = new AuthorizationPolicyBuilder()
                                     .RequireAuthenticatedUser()
                                     .Build();
@@ -27,10 +36,9 @@ namespace JwtLiftoff.Services
             services.Configure<JwtIssuerOptions>(options =>
             {
                 // String options are hardcoded in this sample
-                options.Issuer = "UberJwtServer";
-                options.Audience = "https://localhost/";
-                options.SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.ASCII.GetBytes("TotallySecret")), 
-                    SecurityAlgorithms.HmacSha256);
+                options.Issuer = ISSUER;
+                options.Audience = AUDIENCE;
+                options.SigningCredentials = new SigningCredentials(SigningKey, SecurityAlgorithms.HmacSha256);
             });
         }
 
@@ -40,9 +48,38 @@ namespace JwtLiftoff.Services
             {
                 services.AddAuthorization(options => 
                 {
+                    // Add policies like 'UberPolicy' by 'Policy' => 'Uber'
                     options.AddPolicy($"{name}Policy", policy => policy.RequireClaim("Policy", name));
                 });
             }
         }
+
+        public static void AddJwtAppBuilderConfiguration(IApplicationBuilder app)
+        {
+            app.UseJwtBearerAuthentication(new JwtBearerOptions
+            {
+                AutomaticAuthenticate = true,
+                AutomaticChallenge = true,
+
+                TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = ISSUER,               // Again, hardcoded. Must match with Issuer specified in ConfigureJwt(...)
+
+                    ValidateAudience = true,
+                    ValidAudience = AUDIENCE,           // Again, same
+
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = SigningKey,      // Secret signing key for validation
+
+                    RequireExpirationTime = true,
+                    ValidateLifetime = true,
+
+                    ClockSkew = TimeSpan.Zero
+                }
+            });
+        }
+
+        #endregion
     }
 }
--- src/JwtLiftoff/Startup.cs
+++ src/JwtLiftoff/Startup.cs
@@ -39,6 +39,8 @@ namespace JwtLiftoff
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            ConfigurationService.AddJwtAppBuilderConfiguration(app);
+
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
