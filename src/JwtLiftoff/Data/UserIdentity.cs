c6323175e4e91ec777b66d79f7dc3c5e58be4e81
Gino Messmer <g.messmer@ftsoftware.eu>
2017-03-21 09:48:46 +0100

    JWT package and user model

---
+++ src/JwtLiftoff/Data/UserIdentity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JwtLiftoff.Data
+{
+    /// <summary>
+    /// Simple user data model consisting of username and password
+    /// </summary>
+    public class UserIdentity
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
